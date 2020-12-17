208eb2ef2331a2ad7c143f7536abf830bc42e63a
StevePro7 <steveprozune@hotmail.com>
2020-12-17 20:52:31 +0000

    no message

--- src/doomretro.c
+++ src/doomretro.c
@@ -218,28 +218,28 @@ int main(int argc, char **argv)
     myargc = argc;
     myargv = argv;
 
-#if defined(_WIN32)
-    hInstanceMutex = CreateMutex(NULL, true, PACKAGE_MUTEX);
-
-    if (GetLastError() == ERROR_ALREADY_EXISTS)
-    {
-        if (hInstanceMutex)
-            CloseHandle(hInstanceMutex);
-
-        SetForegroundWindow(FindWindow(PACKAGE_MUTEX, NULL));
-        return 1;
-    }
-
-    // Save the current sticky/toggle/filter key settings so they can be restored later
-    SystemParametersInfo(SPI_GETSTICKYKEYS, sizeof(STICKYKEYS), &g_StartupStickyKeys, 0);
-    SystemParametersInfo(SPI_GETTOGGLEKEYS, sizeof(TOGGLEKEYS), &g_StartupToggleKeys, 0);
-    SystemParametersInfo(SPI_GETFILTERKEYS, sizeof(FILTERKEYS), &g_StartupFilterKeys, 0);
-
-    I_AccessibilityShortcutKeys(false);
-
-    I_SetProcessDPIAware();
-#endif
-
+//#if defined(_WIN32)
+//    hInstanceMutex = CreateMutex(NULL, true, PACKAGE_MUTEX);
+//
+//    if (GetLastError() == ERROR_ALREADY_EXISTS)
+//    {
+//        if (hInstanceMutex)
+//            CloseHandle(hInstanceMutex);
+//
+//        SetForegroundWindow(FindWindow(PACKAGE_MUTEX, NULL));
+//        return 1;
+//    }
+//
+//    // Save the current sticky/toggle/filter key settings so they can be restored later
+//    SystemParametersInfo(SPI_GETSTICKYKEYS, sizeof(STICKYKEYS), &g_StartupStickyKeys, 0);
+//    SystemParametersInfo(SPI_GETTOGGLEKEYS, sizeof(TOGGLEKEYS), &g_StartupToggleKeys, 0);
+//    SystemParametersInfo(SPI_GETFILTERKEYS, sizeof(FILTERKEYS), &g_StartupFilterKeys, 0);
+//
+//    I_AccessibilityShortcutKeys(false);
+//
+//    I_SetProcessDPIAware();
+//#endif
+//
     D_DoomMain();
 
     return 0;
