bf1a4457388902b0934f1bdf73ac4ee33c31b163
StevePro7 <steveprozune@hotmail.com>
2020-12-17 16:26:12 +0000

    no message

--- inc/m_argv.h
+++ inc/m_argv.h
@@ -1,3 +1,4 @@
+
 #if !defined(__M_ARGV_H__)
 #define __M_ARGV_H__
 
@@ -7,13 +8,13 @@
 extern int  myargc;
 extern char **myargv;
 
-//// Returns the position of the given parameter
-//// in the arg list (0 if not found).
-//int M_CheckParm( char *check );
-//
-//// Same as M_CheckParm, but checks that num_args arguments are available
-//// following the specified argument.
-//int M_CheckParmWithArgs( char *check, int num_args, int start );
-//int M_CheckParmsWithArgs( char *check1, char *check2, char *check3, int num_args, int start );
+// Returns the position of the given parameter
+// in the arg list (0 if not found).
+int M_CheckParm( char *check );
+
+// Same as M_CheckParm, but checks that num_args arguments are available
+// following the specified argument.
+int M_CheckParmWithArgs( char *check, int num_args, int start );
+int M_CheckParmsWithArgs( char *check1, char *check2, char *check3, int num_args, int start );
 
 #endif
