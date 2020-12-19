880886be515103f23abad9690f0cb5eace343a6a
StevePro7 <steveprozune@hotmail.com>
2020-12-19 20:48:40 +0000

    no message

--- inc/tables.h
+++ inc/tables.h
@@ -9,13 +9,13 @@
 #define ANGLETOFINESHIFT    19
 
 // Effective size is 10240.
-extern fixed_t  finesine[5 * FINEANGLES / 4];
+extern fixed_t  finesine[ 5 * FINEANGLES / 4 ];
 
 // Re-use data, is just PI/2 phase shift.
 extern fixed_t  *finecosine;
 
 // Effective size is 4096.
-extern fixed_t  finetangent[FINEANGLES / 2];
+extern fixed_t  finetangent[ FINEANGLES / 2 ];
 
 // Binary Angle Measurement, BAM.
 #define ANG1                (ANG45 / 45)
@@ -34,6 +34,6 @@ typedef unsigned int    angle_t;
 // Effective size is 2049;
 // The +1 size is to handle the case when x == y
 //  without additional checking.
-extern angle_t  tantoangle[SLOPERANGE + 1];
+extern angle_t  tantoangle[ SLOPERANGE + 1 ];
 
 #endif
