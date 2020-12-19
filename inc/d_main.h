2df1e703204fa9548ea0b757c3b0fb4326756099
StevePro7 <steveprozune@hotmail.com>
2020-12-19 15:23:27 +0000

    no message

--- inc/d_main.h
+++ inc/d_main.h
@@ -1,21 +1,6 @@
 #if !defined(__D_MAIN_H__)
 #define __D_MAIN_H__
 
-#include "d_event.h"
-#include "doomdef.h"
-
-extern char     **episodes[];
-extern char     **expansions[];
-extern char     **skilllevels[];
-extern char     *packageconfig;
-extern char     *pwadfile;
-extern dboolean advancetitle;
-extern dboolean splashscreen;
-extern dboolean dowipe;
-extern int      titlesequence;
-extern int      fadecount;
-
-void D_Display(void);
 
 //
 // D_DoomMain()
@@ -25,18 +10,5 @@ void D_Display(void);
 //
 void D_DoomMain(void);
 
-// Called by IO functions when input is detected.
-void D_PostEvent(event_t *ev);
-
-//
-// BASE LEVEL
-//
-void D_PageTicker(void);
-void D_PageDrawer(void);
-void D_AdvanceTitle(void);
-void D_DoAdvanceTitle(void);
-void D_StartTitle(int page);
-void D_FadeScreenToBlack(void);
-void D_FadeScreen(void);
 
 #endif
