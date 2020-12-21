16d27aa2496c9d6eae4b155f00c69cbf1f3d9a82
StevePro7 <steveprozune@hotmail.com>
2020-12-21 21:24:22 +0000

    no message

--- src/m_argv.c
+++ src/m_argv.c
@@ -1,4 +1,5 @@
 #include "m_argv.h"
+#include "m_misc.h"
 
 int     myargc;
 char    **myargv;
@@ -10,27 +11,27 @@ char    **myargv;
 // Returns the argument number (1 to argc-1)
 // or 0 if not present
 //
-//int M_CheckParmWithArgs(char *check, int num_args, int start)
-//{
-//    for (int i = start; i < myargc - num_args; i++)
-//        if (M_StringCompare(check, myargv[i]))
-//            return i;
-//
-//    return 0;
-//}
+int M_CheckParmWithArgs(char *check, int num_args, int start)
+{
+    for (int i = start; i < myargc - num_args; i++)
+        if (M_StringCompare(check, myargv[i]))
+            return i;
 
-//int M_CheckParmsWithArgs(char *check1, char *check2, char *check3, int num_args, int start)
-//{
-//    for (int i = start; i < myargc - num_args; i++)
-//        if ((*check1 && M_StringCompare(check1, myargv[i]))
-//            || (*check2 && M_StringCompare(check2, myargv[i]))
-//            || (*check3 && M_StringCompare(check3, myargv[i])))
-//            return i;
-//
-//    return 0;
-//}
-//
-//int M_CheckParm(char *check)
-//{
-//    return M_CheckParmWithArgs(check, 0, 1);
-//}
+    return 0;
+}
+
+int M_CheckParmsWithArgs( char *check1, char *check2, char *check3, int num_args, int start )
+{
+	for( int i = start; i < myargc - num_args; i++ )
+		if( ( *check1 && M_StringCompare( check1, myargv[ i ] ) )
+			|| ( *check2 && M_StringCompare( check2, myargv[ i ] ) )
+			|| ( *check3 && M_StringCompare( check3, myargv[ i ] ) ) )
+			return i;
+
+	return 0;
+}
+
+int M_CheckParm( char *check )
+{
+	return M_CheckParmWithArgs( check, 0, 1 );
+}
