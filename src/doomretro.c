18eb2eb4d10cbc3977ced8f23e6bef4fc2be5851
StevePro7 <steveprozune@hotmail.com>
2020-12-21 21:06:36 +0000

    no message

--- src/doomretro.c
+++ src/doomretro.c
@@ -219,7 +219,7 @@ int main(int argc, char **argv)
     myargv = argv;
 
 //#if defined(_WIN32)
-//    hInstanceMutex = CreateMutex(NULL, true, PACKAGE_MUTEX);
+    hInstanceMutex = CreateMutex(NULL, true, PACKAGE_MUTEX);
 //
 //    if (GetLastError() == ERROR_ALREADY_EXISTS)
 //    {
@@ -231,7 +231,7 @@ int main(int argc, char **argv)
 //    }
 //
 //    // Save the current sticky/toggle/filter key settings so they can be restored later
-//    SystemParametersInfo(SPI_GETSTICKYKEYS, sizeof(STICKYKEYS), &g_StartupStickyKeys, 0);
+    SystemParametersInfo(SPI_GETSTICKYKEYS, sizeof(STICKYKEYS), &g_StartupStickyKeys, 0);
 //    SystemParametersInfo(SPI_GETTOGGLEKEYS, sizeof(TOGGLEKEYS), &g_StartupToggleKeys, 0);
 //    SystemParametersInfo(SPI_GETFILTERKEYS, sizeof(FILTERKEYS), &g_StartupFilterKeys, 0);
 //
